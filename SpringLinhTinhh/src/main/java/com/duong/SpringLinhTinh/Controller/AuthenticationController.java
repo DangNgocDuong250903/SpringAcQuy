6fd8585164048554c210b2409c78be6b1afcd5f8
Duongne <dangngocduong@dtu.edu.vn>
2024-08-05 20:52:42 +0700

    Commit 05/08

--- SpringLinhTinhh/src/main/java/com/duong/SpringLinhTinh/Controller/AuthenticationController.java
+++ SpringLinhTinhh/src/main/java/com/duong/SpringLinhTinh/Controller/AuthenticationController.java
@@ -3,7 +3,10 @@ package com.duong.SpringLinhTinh.Controller;
 import com.duong.SpringLinhTinh.Service.AutheticationService;
 import com.duong.SpringLinhTinh.dto.request.ApiResponse;
 import com.duong.SpringLinhTinh.dto.request.AuthenticationRequest;
+import com.duong.SpringLinhTinh.dto.request.introspecRequest;
 import com.duong.SpringLinhTinh.dto.response.AuthenticationResponse;
+import com.duong.SpringLinhTinh.dto.response.introspecResponse;
+import com.nimbusds.jose.JOSEException;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -12,16 +15,26 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.ParseException;
+
 @RestController
 @RequestMapping("auth")
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class AuthenticationController {
     AutheticationService autheticationService;
-    @PostMapping("/log-in")
+    @PostMapping("/token")
     ApiResponse<AuthenticationResponse> authentication(@RequestBody AuthenticationRequest request){
         var result= autheticationService.authenticate(request);
         return ApiResponse.<AuthenticationResponse>builder()
+                .result(result)
+                .build();
+    }
+    @PostMapping("/introspect")
+    ApiResponse<introspecResponse> authentication(@RequestBody introspecRequest request)
+            throws JOSEException, ParseException {
+        var result= autheticationService.introspect(request);
+        return ApiResponse.<introspecResponse>builder()
                 .result(result)
                 .build();
     }
--- SpringLinhTinhh/src/main/java/com/duong/SpringLinhTinh/Service/AutheticationService.java
+++ SpringLinhTinhh/src/main/java/com/duong/SpringLinhTinh/Service/AutheticationService.java
@@ -1,22 +1,28 @@
 package com.duong.SpringLinhTinh.Service;
 
 import com.duong.SpringLinhTinh.dto.request.AuthenticationRequest;
+import com.duong.SpringLinhTinh.dto.request.introspecRequest;
 import com.duong.SpringLinhTinh.dto.response.AuthenticationResponse;
+import com.duong.SpringLinhTinh.dto.response.introspecResponse;
 import com.duong.SpringLinhTinh.exception.AppException;
 import com.duong.SpringLinhTinh.exception.ErrorCode;
 import com.duong.SpringLinhTinh.repository.UserRepository;
 import com.nimbusds.jose.*;
 import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jose.crypto.MACVerifier;
 import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.experimental.NonFinal;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.info.ProjectInfoAutoConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.text.ParseException;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
@@ -30,10 +36,31 @@ public class AutheticationService {
     UserRepository userRepository;
 
     @NonFinal
-    protected  static final String SIGNER_KEY =
-            "ELjnG67maSPb0PG8ropXO1mUgoVe6q4aDJmbkVmxXv0Z+zn6HtlDts7B4p9D7uUX";
+    @Value("${jwt.signerKey}")
+    protected
+    String SIGNER_KEY;
 
-    private final ProjectInfoAutoConfiguration projectInfoAutoConfiguration;
+    public introspecResponse introspect(introspecRequest request)
+            throws JOSEException, ParseException {
+        var token = request.getToken();
+        JWSVerifier verifier = new MACVerifier(SIGNER_KEY.getBytes());
+        // Tao JWSVerifier de kiem tra token
+
+        SignedJWT signedJWT = SignedJWT.parse(token);
+        // Parse token thanh SignedJWT
+
+        Date expityTime = signedJWT.getJWTClaimsSet().getExpirationTime();
+        // Lay thoi gian het han cua token
+
+        var verified = signedJWT.verify(verifier); // Kiem tra token: true -> false
+
+
+        return introspecResponse
+                .builder()
+                .valid(verified && expityTime.after(new Date()))
+                // Neu token hop le va chua het han thi tra ve true, nguoc lai tra ve false
+                .build();
+    }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
         PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
@@ -60,11 +87,10 @@ public class AutheticationService {
         // Tao JWSHeader de chua thong tin ve thuat toan ma hoa
             JWTClaimsSet jwtClaimsSet= new JWTClaimsSet.Builder()
                     //subject: thông tin về đối tượng mà token đại diện (trong trường hợp này là username).
-                    //issuer: nơi phát hành token (ở đây là "devteria.com").
                     //issueTime: thời gian phát hành token.
                     //expirationTime: thời gian hết hạn của token (được tính là 1 giờ từ thời điểm hiện tại).
                     .subject(username)
-                    .issuer("devteria.com")
+                    .issuer("devteria.com") //Dinh danh cau hinh
                     .issueTime(new Date())
                     .expirationTime(new Date(
                             // Tao token co thoi gian song la 1h
@@ -76,13 +102,13 @@ public class AutheticationService {
             Payload payload = new Payload(jwtClaimsSet.toJSONObject());
             // Tao Payload de chua JWTClaimsSet
             JWSObject jwsObject = new JWSObject(header,payload);
-
             //Sử dụng khóa ký (SIGNER_KEY) để ký jwsObject.
             //Nếu ký thành công, trả về token dưới dạng chuỗi.
             //Nếu có lỗi trong quá trình ký, ghi lại lỗi và ném ngoại lệ RuntimeException.
             try {
                 jwsObject.sign(new MACSigner(SIGNER_KEY.getBytes()));
                 return jwsObject.serialize();
+                //serialize() chuyển JWSObject thành chuỗi
             } catch (JOSEException e) {
                 log.error("Cannot create token", e);
                 throw new RuntimeException(e);
