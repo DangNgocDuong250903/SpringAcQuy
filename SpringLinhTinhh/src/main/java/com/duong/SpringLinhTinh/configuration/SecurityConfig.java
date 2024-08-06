f580317aa1607bac1eaf22ad6f311962eed95faa
Duongne <dangngocduong@dtu.edu.vn>
2024-08-06 11:42:34 +0700

    Security config 06/08

---
+++ SpringLinhTinhh/src/main/java/com/duong/SpringLinhTinh/configuration/SecurityConfig.java
@@ -0,0 +1,69 @@
+package com.duong.SpringLinhTinh.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+@Configuration
+@EnableWebSecurity
+
+public class SecurityConfig {
+
+    private final String[] PUBLIC_ENDPOINTS={"/users","/auth/token","/auth/introspect"};
+        // Cac endpoint public
+    @Value("${jwt.signerKey}")
+    private String SigningKey;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeRequests(requests ->
+                        requests.requestMatchers(HttpMethod.POST,PUBLIC_ENDPOINTS).permitAll()
+        // Cho phep tat ca cac request POST den cac endpoint PUBLIC_ENDPOINTS
+                                .anyRequest().authenticated());
+        // Đối với mọi yêu cầu khác, yêu cầu người dùng phải được xác thực
+
+        httpSecurity.oauth2ResourceServer(oath2 ->
+        //Cấu hình để sử dụng OAuth2 Resource Server.
+                oath2.jwt(jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder()))
+        // Cấu hình OAuth2 Resource Server để sử dụng JWT, và thiết lập bộ giải mã JWT
+                );
+
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+        // Disable CSRF neu khong thi khong the hien thi du lieu tren trinh duyet
+
+        return httpSecurity.build();
+    }
+
+
+    @Bean
+    JwtDecoder jwtDecoder(){
+        SecretKeySpec secretKeySpec = new SecretKeySpec(SigningKey.getBytes(), "HmacSHA");
+        return NimbusJwtDecoder
+                .withSecretKey(secretKeySpec)
+                .macAlgorithm(MacAlgorithm.HS512)
+                .build();
+        // Tao ra mot doi tuong JwtDecoder de giai ma token
+        // MacAlgorithm.HS512: Su dung thuat toan HS512 de giai ma token
+    };
+
+}
+
+
+//Các endpoint công khai có thể được truy cập mà không cần xác thực.
+//Các yêu cầu khác yêu cầu xác thực.
+//Sử dụng OAuth2 Resource Server với JWT để xác thực.
+//Vô hiệu hóa CSRF để tránh lỗi khi hiển thị dữ liệu trên trình duyệt.
+//Sử dụng khóa ký từ file cấu hình và giải mã JWT sử dụng thuật toán HS512.
