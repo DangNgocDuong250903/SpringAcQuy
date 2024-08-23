3e0552b45b166c875aa0543ecf6fd5fc4686df0d
Đặng Ngọc Dương <94966949+DangNgocDuong250903@users.noreply.github.com>
2024-08-23 16:12:58 +0700

    Delete SpringLinhTinhh directory

--- SpringLinhTinhh/src/main/java/com/duong/SpringLinhTinh/repository/PermissionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.duong.SpringLinhTinh.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.duong.SpringLinhTinh.entity.Permission;
-@Repository
-public interface PermissionRepository extends JpaRepository<Permission,String> {
-}
